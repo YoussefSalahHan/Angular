a21f6db496f4ab6e271770195f0501980f81ffea
youssef <y.salah@orangestudio.com>
2017-11-09 13:36:43 +0200

    disqus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DisqusModule } from "ngx-disqus";
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DisqusModule.forRoot('disqus_shortname')
   ],
   providers: [],
   bootstrap: [AppComponent]
